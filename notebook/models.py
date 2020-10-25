44bee9fec242c5405d74baf2ac70b8314eca9e52
Phurinat Sumateyajarn <pongpong_151@hotmail.com>
2020-10-25 22:33:24 +0700

    add register and databaseuser

--- notebook/models.py
+++ notebook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class UserUn(models.Model):
+    firstname = models.CharField(max_length = 64)
+    lastname = models.CharField(max_length = 64)
+    email = models.EmailField(max_length = 64)
+    password = models.CharField(max_length = 32)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} Email:{self.email}"
+    
--- notebook/views.py
+++ notebook/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from .models import UserUn
+from django.contrib.auth.models import User
 def index(request):
     return render(request,'notebook/index.html')
 def about(request):
     return render(request,'notebook/about.html')
+def register(request):
+    return render(request,'notebook/register.html')
+
+def addregister(request):
+    if request.method == 'POST':
+        name = request.POST['firstname']
+        surname = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmpassword = request.POST['confirmpassword']
+        
+        if password == confirmpassword:
+            userun = UserUn.objects.create(
+            firstname = name,
+            lastname = surname,
+            email = email,
+            password = password,
+            )
+            user = User.objects.create_user(
+            first_name = name,
+            last_name = surname,
+            email = email,
+            password = password,
+            )
+            userun.save()
+            user.save()
+            return render(request,'notebook/login.html')
+        else:
+            return render(request,'notebook/register.html')
+    return render(request,'notebook/register.html')
