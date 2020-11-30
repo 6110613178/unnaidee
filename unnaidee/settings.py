a6adc72041859d669daad1f18aff0eca26c3543a
Eknarin Lernuntawat <eknarin.120643@gmail.com>
2020-11-30 15:09:05 +0700

    final v.2

--- unnaidee/settings.py
+++ unnaidee/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,4 +121,4 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
